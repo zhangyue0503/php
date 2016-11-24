f2672d6d412f1ffec38071e84686873b29db8e15
zhangyue0503 <zhangyue0503@hotmail.com>
2016-11-24 13:31:32 +0800

    20161124

---
+++ phphexinjishuyuzuijiashijian/7.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhangyue
+ * Date: 2016/11/24
+ * Time: 下午12:24
+ */
+
+echo "helloworld\r";
+$data['first'] = "Hello";
+$data['first']['second'] = 'world';
+echo $data['first'];
