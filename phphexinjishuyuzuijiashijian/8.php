a910485ca4bed554b745856298b5bca7f00b662f
zhangyue0503 <zhangyue0503@hotmail.com>
2016-11-25 14:47:21 +0800

    20161125

---
+++ phphexinjishuyuzuijiashijian/8.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhangyue
+ * Date: 2016/11/25
+ * Time: 下午2:44
+ */
+
+$mc = new Memcache();
+$mc->connect('127.0.0.1',11211);
+$mc->set('key','value',0,10);
+$val = $mc->get('key');
+$mc->delete('key');
+$mc->flush();
+$mc->close();
