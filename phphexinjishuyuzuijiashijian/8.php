0fb1ec1ccc2a1d7b5e73ea9a05012d873694a672
zhangyue0503 <zhangyue0503@hotmail.com>
2016-11-25 14:52:42 +0800

    20161125

--- phphexinjishuyuzuijiashijian/8.php
+++ phphexinjishuyuzuijiashijian/8.php
@@ -6,8 +6,8 @@
  * Time: 下午2:44
  */
 
-$mc = new Memcached();
-$mc->connect('127.0.0.1',11211);
+$mc = new emcached();
+$mc->addServer('127.0.0.1',11211);
 $mc->set('key','value',0,10);
 $val = $mc->get('key');
 $mc->delete('key');
