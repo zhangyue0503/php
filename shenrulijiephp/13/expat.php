5c32ecdaf97d65adcd0bc423150d03cb2927a33a
zhangyue0503 <zhangyue0503@hotmail.com>
2017-01-10 12:35:27 +0800

    20170110

--- shenrulijiephp/13/expat.php
+++ shenrulijiephp/13/expat.php
@@ -5,3 +5,59 @@
  * Date: 2017/1/10
  * Time: 下午12:03
  */
+
+function handle_open_element($p,$element,$attributes){
+    switch ($element){
+        case 'BOOK':
+            echo '<div>';
+            break;
+        case 'CHAPTER':
+            echo "<p>Chapter {$attributes['NUMBER']}：";
+            break;
+        case 'COVER':
+            $image = @getimagesize($attributes['FILENAME']);
+            echo "<img src=\"{$attributes['FILENAME']}\" $image[3] border=\"0\"><br>";
+            break;
+        case 'TITLE':
+            echo '<h2>';
+            break;
+        case 'YEAR':
+        case 'AUTHOR':
+        case 'PAGES':
+            echo '<span class="label">'.$element.'</span>';
+            break;
+    }
+}
+function handle_close_element($p,$element){
+    switch ($element){
+        case 'BOOK':
+            echo '</div>';
+            break;
+        case 'CHAPTER':
+            echo "</p>";
+            break;
+        case 'TITLE':
+            echo '</h2>';
+            break;
+        case 'YEAR':
+        case 'AUTHOR':
+        case 'PAGES':
+            echo '<br>';
+            break;
+    }
+}
+function handle_character_data($p,$cdata){
+    echo $cdata;
+}
+
+$p = xml_parser_create();
+
+xml_set_element_handler($p,'handle_open_element','handle_close_element');
+xml_set_character_data_handler($p,'handle_character_data');
+
+$file = 'books4.xml';
+$fp = @fopen($file,'r') or die("<p>Could not open a file called '$file'.</p>");
+while($data = fread($fp,4096)){
+    xml_parse($p,$data,feof($fp));
+}
+xml_parser_free($p);
--- shenrulijiephp/13/simplexml.php
+++ shenrulijiephp/13/simplexml.php
@@ -5,3 +5,44 @@
  * Date: 2017/1/10
  * Time: 下午12:18
  */
+
+$xml = simplexml_load_file('books4.xml');
+
+foreach($xml->book as $book){
+    echo "<div><h2>$book->title";
+
+    if(isset($book->title['edition'])){
+        echo " (Edition {$book->title['edition']})";
+    }
+
+    echo "</h2>";
+
+    foreach($book->author as $author){
+        echo "<span class=\"label\">Author</span>:$author<br/>";
+    }
+    echo "<span class=\"label\">Published:</span> $book->year<br/>";
+
+    if(isset($book->pages)){
+        echo "<span class=\"label\">Pages:</span> $book->pages<br/>";
+    }
+
+    if(isset($book->chapter)){
+        echo 'Table of Contents<ul>';
+        foreach($book->chapter as $chapter){
+            echo '<li>';
+            if(isset($chapter['number'])){
+                echo "Chapter {$chapter['number']}:";
+            }
+            echo $chapter;
+            if(isset($chapter['pages'])){
+                echo " {$chapter['pages']} Pages";
+            }
+            echo '</li>';
+        }
+    }
+    if(isset($book->cover)){
+        $img = @getimagesize($book->cover['filename']);
+        echo "<img src=\"{$book->cover['filename']}\" $image[3] border='0'><br>";
+    }
+    echo '</div>';
+}
