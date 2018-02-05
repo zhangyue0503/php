a3ac1f1124f532a9fe2413756e19523b32468704
zhangyue <zhangyue@dyrs.com.cn>
2018-02-05 18:43:26 +0800

    20180205

---
+++ swoole/websocketserver.php
@@ -0,0 +1,18 @@
+<?php
+
+$server = swoole_websocket_server('127.0.0.1', 9502);
+$server->on("open",function($server,$req){
+    echo "connect open: {$req->fd}\n";
+});
+
+$server->on("message",function($server,$frame){
+    echo "received message: {$frame->data}\n";
+    $server->push($frame->fd,json_encode(["hello", "world"]));
+});
+
+$server->on("close",function($server,$fd){
+    echo "connection close: {$fd}\n";
+});
+
+$server->start();
+
