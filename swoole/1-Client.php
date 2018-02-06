1941dfd73610721926d280b27028ed63457ea872
zhangyue <zhangyue@dyrs.com.cn>
2018-02-06 14:41:51 +0800

    20180206

---
+++ swoole/1-Client.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/2/6
+ * Time: 14:38
+ */
+class Client
+{
+    private $client;
+
+    public function __construct()
+    {
+        $this->client = new swoole_client(SWOOLE_SOCK_TCP);
+    }
+
+    public function connect()
+    {
+        if (!$this->client->connect("127.0.0.1", 9501, 1)) {
+            echo "Error: {$fp->errMsg}[{$fp->errCode}]\n";
+        }
+        $message = $this->client->recv();
+        echo "Get Message From Server:{$message}\n";
+
+        fwrite(STDOUT, "请输入消息：");
+        $msg = trim(fgets(STDIN));
+        $this->client->send($msg);
+    }
+}
+
+$client = new Client();
+$client->connect();
