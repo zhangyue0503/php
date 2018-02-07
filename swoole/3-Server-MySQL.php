053d1e1dc3212bcfb7bb57e6f5968b411c95c63e
zhangyue <zhangyue@dyrs.com.cn>
2018-02-07 18:05:33 +0800

    20180207

--- swoole/3-Server-MySQL.php
+++ swoole/3-Server-MySQL.php
@@ -80,9 +80,9 @@ class Server
         echo "Get Message From Client {$fd}:{$data}\n";
 
         $sql = [
-            'sql'   => 'Insert into test values(pid=?,name=?)',
+            'sql'   => 'Insert into test values(name=?)',
             'param' => [
-                0, "'name'"
+                "'name'"
             ],
             'fd'    => $fd
         ];
