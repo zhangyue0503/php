c8342cd0115b91996c1293524aebd7ca4f629a50
zhangyue <zhangyue@dyrs.com.cn>
2018-02-07 17:37:53 +0800

    20180207

--- swoole/3-Server-MySQL.php
+++ swoole/3-Server-MySQL.php
@@ -66,7 +66,7 @@ class Server
             $this->pdo = new PDO(
                 "mysql:host=localhost;port=3306;dbname=Test",
                 "root", "", [
-                    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8';",
+//                    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8';",
                     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
                     PDO::ATTR_PERSISTENT         => true
 
