304544da188c270e5a9a46348fb48db7401b6a4a
zhangyue <zhangyue@dyrs.com.cn>
2018-02-08 14:41:59 +0800

    20180208

--- swoole/4-Server.php
+++ swoole/4-Server.php
@@ -41,6 +41,7 @@ class Server
     {
         $info = $serv->connection_info($fd, $from_id);
         //来自9502的内网管理接口
+        print_r($info);
         if ($info['from_port'] == 9502) {
             $serv->send($fd, "welcome admin\n");
         } else {
