5929e3e16f3d44387bab8053c3b23d9737d0446a
zhangyue <zhangyue@dyrs.com.cn>
2018-02-08 16:05:18 +0800

    20180208

---
+++ swoole/4-Server-After.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/2/6
+ * Time: 9:55
+ */
+//https://github.com/LinkedDestiny/swoole-doc/blob/master/03.Timer定时器、心跳检测及Task进阶实例：mysql连接池.md
+
+class Server
+{
+    private $serv;
+
+    public function __construct()
+    {
+        $this->serv = new swoole_server('0.0.0.0', 9501);
+        $this->serv->set([
+            'worker_num'    => 8,
+            'daemonize'     => false,
+            'max_request'   => 10000,
+            'dispatch_mode' => 2,
+            'debug_mode'    => 1,
+        ]);
+
+        $this->serv->on('Start', [$this, 'onStart']);
+        $this->serv->on('Connect', [$this, 'onConnect']);
+        $this->serv->on('Receive', [$this, 'OnReceive']);
+        $this->serv->on('Close', [$this, 'onClose']);
+        $this->serv->on('Timer', [$this, 'onTimer']);//新版本已经不用addtimer方法了
+
+        $this->serv->start();
+    }
+
+    public function onTimer($serv, $interval)
+    {
+        echo 111;
+    }
+
+
+    public function onStart($serv, $worker_id)
+    {
+        echo "Start\n";
+    }
+
+    public function onReceive(swoole_server $serv, $fd, $from_id, $data)
+    {
+        echo "Get Message From Client {$fd}:{$data}\n";
+
+        $param = [
+            'fd'  => $fd,
+            'msg' => $data
+        ];
+        $str = json_encode($param);
+        $serv->after(1000, [$this, 'onAfter'], $str);
+    }
+
+    public function After($data)
+    {
+        $param = json_decode($data, true);
+        $this->serv->send($param['fd'], $param['msg']);
+    }
+
+    public function onConnect($serv, $fd, $from_id)
+    {
+        echo "Client {$fd} connect\n";
+    }
+
+    function onClose($serv, $fd, $from_id)
+    {
+        echo "Client {$fd} close connection\n";
+    }
+}
+
+$server = new Server();
