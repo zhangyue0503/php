1e5e31598347237a3d20d42aa4b0fff5b8c2efef
JSQ <1104144308@qq.com>
2019-03-31 10:48:26 +0800

    phpstatic

--- newblog/php-static.php
+++ newblog/php-static.php
@@ -33,18 +33,24 @@ $test::showV();
 // 初始化特性
 class Calculate
 {
-    function cacl(){
+    function cacl()
+    {
         static $a = 1;
         echo $a;
         $a++;
     }
-    static function cacl2(){
+
+    static function cacl2()
+    {
         static $a = 1;
         echo $a;
         $a++;
     }
+
     static $b = 1;
-    static function cacl3(){
+
+    static function cacl3()
+    {
         echo self::$b;
         self::$b++;
     }
@@ -72,34 +78,40 @@ function test()
     }
     $count--;
 }
+
 test();
 
 
 // 引用对象问题
 // 引用不是静态地存储
-class Foo{
+class Foo
+{
     public $a = 1;
 }
-function getRefObj($o){
+
+function getRefObj($o)
+{
     static $obj;
     var_dump($obj);
-    if(!isset($obj)){
+    if (!isset($obj)) {
         $obj = &$o;
     }
     $obj->a++;
     return $obj;
 }
 
-function getNoRefObj($o){
+function getNoRefObj($o)
+{
     static $obj;
     var_dump($obj);
-    if(!isset($obj)){
+    if (!isset($obj)) {
         $obj = $o;
     }
     $obj->a++;
     return $obj;
 }
-$o = new Foo;
+
+$o    = new Foo;
 $obj1 = getRefObj($o);
 $obj2 = getRefObj($o);
 
@@ -108,63 +120,88 @@ $obj4 = getNoRefObj($o);
 
 // 后期静态绑定
 // self取决于当前定义方法所在的类
-class A {
-    static function who(){
-        echo __CLASS__."\n";
+class A
+{
+    static function who()
+    {
+        echo __CLASS__ . "\n";
     }
-    static function test(){
+
+    static function test()
+    {
         self::who();
     }
 }
-class B extends A{
-    static function who(){
-        echo __CLASS__."\n";
+
+class B extends A
+{
+    static function who()
+    {
+        echo __CLASS__ . "\n";
     }
 }
+
 B::test(); // A
 
 // static 表示运行最初时的类，不是方法定义时的类
 // 在非静态环境下，所调用的类即为该对象实例所属的类。由于 $this-> 会在同一作用范围内尝试调用私有方法，而 static:: 则可能给出不同结果。另一个区别是 static:: 只能用于静态属性。
-class AA {
-    static function who(){
-        echo __CLASS__."\n";
+class AA
+{
+    static function who()
+    {
+        echo __CLASS__ . "\n";
     }
-    static function test(){
+
+    static function test()
+    {
         static::who();
     }
 }
-class BB extends AA{
-    static function who(){
-        echo __CLASS__."\n";
+
+class BB extends AA
+{
+    static function who()
+    {
+        echo __CLASS__ . "\n";
     }
 }
+
 BB::test(); // BB
 
 // parent和self会转发调用信息
-class AAA {
-    public static function foo() {
+class AAA
+{
+    public static function foo()
+    {
         static::who();
     }
 
-    public static function who() {
-        echo __CLASS__."\n";
+    public static function who()
+    {
+        echo __CLASS__ . "\n";
     }
 }
 
-class BBB extends AAA {
-    public static function test() {
+class BBB extends AAA
+{
+    public static function test()
+    {
         AAA::foo();
         parent::foo();
         self::foo();
     }
 
-    public static function who() {
-        echo __CLASS__."\n";
+    public static function who()
+    {
+        echo __CLASS__ . "\n";
     }
 }
-class CCC extends BBB {
-    public static function who() {
-        echo __CLASS__."\n";
+
+class CCC extends BBB
+{
+    public static function who()
+    {
+        echo __CLASS__ . "\n";
     }
 }
 
